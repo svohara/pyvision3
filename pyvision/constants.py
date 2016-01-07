64e07cac3746a7ebdd63340abf8276878aecabc5
svohara <svohara@gmail.com>
2016-01-07 16:14:09 -0700

    Added setup.py, modifications to data directory and constants to be compatible with install egg distributions

--- pyvision/constants.py
+++ pyvision/constants.py
@@ -5,18 +5,18 @@ Created on Dec 18, 2015
 """
 import os
 import inspect
-
+from pkg_resources import resource_filename, Requirement
 import pyvision as pv3
 
 PACKAGE_DIR = os.path.dirname(inspect.getfile(pv3))
-SAMPLE_DATA_DIR = os.path.join(PACKAGE_DIR, "..", "data")
 
 # It's useful to have some built-in constants referring to
 # sample data files to make demonstrations and tests easy.
-IMG_DRIVEWAY = os.path.join(SAMPLE_DATA_DIR, "driveway.jpg")
-IMG_PRIUS = os.path.join(SAMPLE_DATA_DIR, "prius_gray.jpg")
-IMG_SLEEPYCAT = os.path.join(SAMPLE_DATA_DIR, "sleepycat.jpg")
-VID_PRIUS = os.path.join(SAMPLE_DATA_DIR, "prius_movie.mov")
+#tmp = Requirement.parse(__name__)
+IMG_DRIVEWAY = resource_filename("pyvision", "data/driveway.jpg")
+IMG_PRIUS = resource_filename("pyvision", "data/prius_gray.jpg")
+IMG_SLEEPYCAT = resource_filename("pyvision", "data/sleepycat.jpg")
+VID_PRIUS = resource_filename("pyvision", "data/prius_movie.mov")
 
 if __name__ == '__main__':
     pass
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyvision3",
+    version="0.1",
+    packages=find_packages(),
+    url="https://github.com/svohara/pyvision3",
+    license="MIT",
+    author="Stephen O'Hara",
+    author_email="svohara@gmail.com",
+    description="Facilitates computer vision research and prototyping using python and openCV",
+    package_data={
+        # include all files found in the 'data' subdirectory
+        # of the 'pyvision' package
+        'pyvision': ['data/*']
+    },
+    test_suite="tests"
+)
