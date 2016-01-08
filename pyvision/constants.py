a992ba41abf0558a4fd7821a3f8757dbfbd76e5e
svohara <svohara@gmail.com>
2016-01-07 17:07:25 -0700

    Removed an unused import.

--- pyvision/constants.py
+++ pyvision/constants.py
@@ -5,14 +5,13 @@ Created on Dec 18, 2015
 """
 import os
 import inspect
-from pkg_resources import resource_filename, Requirement
+from pkg_resources import resource_filename
 import pyvision as pv3
 
 PACKAGE_DIR = os.path.dirname(inspect.getfile(pv3))
 
 # It's useful to have some built-in constants referring to
 # sample data files to make demonstrations and tests easy.
-#tmp = Requirement.parse(__name__)
 IMG_DRIVEWAY = resource_filename("pyvision", "data/driveway.jpg")
 IMG_PRIUS = resource_filename("pyvision", "data/prius_gray.jpg")
 IMG_SLEEPYCAT = resource_filename("pyvision", "data/sleepycat.jpg")
