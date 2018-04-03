5d69e7f5778b9aade4a6c5ca8a533738eb7e5ea5
svohara <svohara@gmail.com>
2018-04-02 20:05:35 -0600

    Tweaks to prepare for publishing to pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join('pyvision', 'data', 'project_info.json'), 'r') as infile:
 _version = ".".join([str(x) for x in _info["version_tuple"]])
 
 setup(
-    name="pyvision3",
+    name="pyvision",
     version=_version,
     packages=find_packages(),
     url=_info["url"],
@@ -21,5 +21,8 @@ setup(
         # of the 'pyvision' package
         'pyvision': ['data/*']
     },
-    test_suite="tests"
+    test_suite="tests",
+    download_url="{}/archive/{}.tar.gz".format(_info["url"], _version),
+    keywords=["images", "video", "computer vision"],
+    classifiers=[],
 )
